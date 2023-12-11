4c4dbc1c05d7693c69370657c5de8e1be6033c3c
Zhou Chang <achang.zhou@gmail.com>
2023-12-11 16:26:09 +0800

    clean code for debug DCU runtime

---
+++ build_env.sh
@@ -0,0 +1,7 @@
+### sentencepiece
+export VT_SOURCE=/public/home/ac56jm0dor/workspace/vt-transformer
+export SENTENCEPIECE_DIR=/public/home/ac56jm0dor/opt/sentencepiece
+
+### DCU 
+export DTK_DIR=/public/home/ac56jm0dor/opt/dtk
+
