a575b5fc30c22b15c53f841f0acbb3c4bf332d1c
Zhou Chang <achang.zhou@gmail.com>
2024-06-07 14:02:32 +0800

    clean code

--- models/qwen2-7b/chat.cpp
+++ models/qwen2-7b/chat.cpp
@@ -14,6 +14,11 @@
 
 #include "memory.hpp"
 
+const char* init_cmd = "gpu_init";
+const char* main_cmd = "gpu_main";
+const size_t max_input = 512;
+const size_t max_output = 512;
+
 inline std::string fileToString(const char* filename) {
     std::ifstream t(filename);
     if ( ! t.is_open() ) {
@@ -33,28 +38,20 @@ inline std::string fileToString(const char* filename) {
 }
 
 struct ChatApplication {
-    ChatApplication(vt::ComputingContext* ctx): ctx_(ctx) {
+    ChatApplication(vt::Enviroment* env) : env_(env) {
         tokenizer_ = vt::build_tokenizer_qwen("./qwen.tiktoken");
+        main_code_ = env_->build(main_cmd);
     }
     ~ChatApplication() {
         delete tokenizer_;
+        delete main_code_;
     }
 
     void write_all(const void* buf, size_t nbyte) {
-        vt_assert( ctx_->pipe_write(1, buf, nbyte) > 0, "write_all error");
+        vt_assert( env_->ctx()->pipe_write(0, buf, nbyte) > 0, "write_all error");
     }
-
-    void wait_all_ready() {
-        int dummy = -1;
-        ctx_->pipe_read(&dummy, sizeof(int));
-        vt_assert(dummy == 1, "wait_all_ready error");
-    }
-
-    const size_t max_input = 512;
-    const size_t max_output = 512;
 
     void run() {
-        wait_all_ready();
 
         std::list<std::string>  history;
         history.push_back("<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n");
@@ -90,13 +87,15 @@ struct ChatApplication {
             for ( int t = 0; t < (int)max_output; t++) {
                 int batch = 1;
                 int len = id.size();
-                write_all(&batch, sizeof(int));
-                write_all(&len, sizeof(int));
                 write_all(id.data(), id.size() * sizeof(int));
                 write_all(mask.data(), mask.size() * sizeof(int));
 
+                env_->stack().push_number(batch);
+                env_->stack().push_number(len);
+                env_->run(main_code_);
+
                 next = -1;
-                ctx_->pipe_read(&next, sizeof(int));
+                env_->ctx()->pipe_read(&next, sizeof(int));
                 if ( next == tokenizer_->token_eos() ) {
                     break;
                 }
@@ -121,9 +120,6 @@ struct ChatApplication {
             talk = talk + "<|im_end|>\n";
             history.push_back(talk);
         }
-
-        int n = -1;
-        write_all(&n, sizeof(int));
     }
 
     std::vector<int> build_from_history(const std::list<std::string>& history) {
@@ -152,7 +148,8 @@ struct ChatApplication {
 
 private:
     vt::Tokenizer* tokenizer_;
-    vt::ComputingContext* ctx_;
+    vt::Enviroment* env_;
+    vt::DaG* main_code_;
 };
 
 void do_inference(vt::Enviroment* env, const char* dag_file) {
@@ -201,44 +198,45 @@ void do_inference(vt::Enviroment* env, const char* dag_file) {
     delete target_cmd;
 }
 
-int main(int argc, char* argv[] ) {
+
+int main(int argc, const char* argv[] ) {
     setlocale(LC_ALL, "");
     if ( argc < 2 ) {
-        std::cout << "usage: ./chat [dag_file] " << std::endl;
+        std::cout << "usage: ./chat [dag_files] " << std::endl;
         return -1;
     }
     vt::ComputingContext* ctx = new vt::ComputingContext();
-    ctx->boot_host(1);
-
-    if ( ctx->pipe_rank == 0) {
-        ChatApplication* app = new ChatApplication(ctx);
-        app->run();
-
-        delete app;
-        delete ctx;
-
-        // wait for all child processes finished
-        {
-            int status = 0;
-            while ( wait(&status) != -1) {
-            }
-        }
-    } else if ( ctx->pipe_rank == 1) {
+    ctx->boot_host(0);
 #ifdef _USING_DEVICE_CUDA_
-        ctx->boot_cuda(0);
+    ctx->boot_cuda(0);
 #endif
+    vt::Enviroment* env = new vt::Enviroment(ctx);
+    env->insert_native_word("app.mem", MemoryCounting::creator);
+    env->insert_native_word("app.align", MemoryAlign::creator);
 
-#ifdef _USING_DEVICE_HIP_
-        ctx->boot_hip(1);
+    // load whole code
+    {
+        std::string all_code;
+        for(int i = 1; i < argc; i++) {
+            const char* dag_file = argv[i];
+            all_code += fileToString(dag_file);
+        }
+
+        vt::DaG* init_bin = env->build(all_code);
+#ifdef _USING_DEVICE_CUDA_
+        env->stack().push_string("cuda");
 #endif
-
-        vt::Enviroment* env = new vt::Enviroment(ctx);
-        env->insert_native_word("app.mem", MemoryCounting::creator);
-        env->insert_native_word("app.align", MemoryAlign::creator);
-        do_inference(env, argv[1]);
-
-        delete env;
-        delete ctx;
+        env->run(init_bin);
+        delete init_bin;
     }
+    env->execute(init_cmd);
+    
+    ChatApplication* app = new ChatApplication(env);
+    app->run();
+
+    delete app;
+    delete env;
+    delete ctx;
 }
+
 
