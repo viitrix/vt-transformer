d0f1ffd2b267aed09f146d22e646c2461c83318c
Zhou Chang <achang.zhou@gmail.com>
2024-06-20 07:14:54 +0800

    prepared for a simple web client

--- models/qwen2-7b/http.cpp
+++ models/qwen2-7b/http.cpp
@@ -1,62 +1,78 @@
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include <tuple>
 #include <list>
 #include <unistd.h>
+#include <unordered_map>
 #include <sys/wait.h>
-#include <atomic>
-#include <condition_variable>
-#include <queue>
-#include <mutex>
-#include <sstream>
-#include <thread>
-
-#define CPPHTTPLIB_OPENSSL_SUPPORT
-#include <httplib.h>
+#include <locale.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #include <tokenizer_combo.hpp>
 #include <tensortype_inc.hpp>
 
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include <httplib.h>
+
 #include "memory.hpp"
 
-const size_t MEM_CTX_SIZE = 4 * 1024 * 1024 * 1024l;
+inline std::string fileToString(const char* filename) {
+    std::ifstream t(filename);
+    if ( ! t.is_open() ) {
+        std::cout << "Can't open " << filename << std::endl;
+        vt_panic("Can't open file");
+    }
+
+    std::string str;
+    t.seekg(0, std::ios::end);
+    str.reserve(t.tellg());
+    t.seekg(0, std::ios::beg);
+
+    str.assign((std::istreambuf_iterator<char>(t)),
+        std::istreambuf_iterator<char>());
+
+    return str;
+}
+
+const char* init_cmd = "gpu_init";
+const char* main_cmd = "gpu_main";
+
+const size_t max_input = 1024;
+const size_t max_output = 1024;
 
 struct ChatApplication {
-    ChatApplication() {
-        tokenizer_ = vt::build_tokenizer_qwen("./qwen.tiktoken");
-        inferencing_ = false;
+    ChatApplication(vt::Enviroment* env) : env_(env) {
+        tokenizer_ = vt::build_tokenizer_qwen("./qwen.tiktoken", true);
+        target_code_ = env_->build(main_cmd);
+
+        svr_ = new httplib::Server();
     }
     ~ChatApplication() {
+        svr_->stop();
+        delete svr_;
+
+        delete target_code_;
         delete tokenizer_;
     }
 
     void write_all(const void* buf, size_t nbyte) {
-        vt_assert( vt::CollectiveContext::pipe_write(1, buf, nbyte) > 0, "write_all error");
+        vt_assert( env_->ctx()->pipe_write(0, buf, nbyte) > 0, "write_all error");
     }
 
-    void wait_all_ready() {
-        int dummy = -1;
-        vt::CollectiveContext::pipe_read(&dummy, sizeof(int));
-        vt_assert(dummy == 1, "wait_all_ready error");
-    }
+    const char* system_talk =
+        "<|im_start|>system\n"
+        "你现在开始扮演一个知识小助手。"
+        "<|im_end|>\n<|im_start|>user\n";
 
-    const size_t max_input = 2048;
-    const size_t max_output = 1024;
+    void run() {
+        svr_->set_mount_point("/", "./www");
 
-    static void server(ChatApplication* app, httplib::Server& svr) {
-        svr.Post("/chat", [&](const httplib::Request &req, httplib::Response &res, const httplib::ContentReader &content_reader) {
-            if ( app->inferencing_ == true ) {
-                res.set_content("{'error': 'System is busy!'}\r\n\r\n", "application/json");
-                return;
-            }
+        svr_->Post("/chat", [&](const httplib::Request &req, httplib::Response &res, const httplib::ContentReader &content_reader) {
             if (req.is_multipart_form_data()) {
                 res.set_content("{'error': 'Don't support multipart post!'}\r\n\r\n", "application/json");
                 return;
-            }
-            if (req.has_param("reset_cache")) {
-                if ( req.get_param_value("reset_cache") == "true" ) {
-                    app->writeline("!");
-                }
             }
 
             std::string body;
@@ -65,99 +81,84 @@ struct ChatApplication {
                     return true;
                 });
 
-            app->writeline(body);
+            std::string result;
+            auto s = inference(body, result);
+            if ( s == 0 ) {
+                result = result + "\r\n\r\n";
+                res.set_content(result, "text/plain");
+                return;
+            }
 
-            res.set_chunked_content_provider("text/event-stream", [&](size_t offset, httplib::DataSink &sink) {
-                    std::string one_token = app->readtoken();
-                    if ( one_token == "" ) {
-                        sink.write("\r\n\r\n", 4);
-                        sink.done();
-                        return true;
-                    }
-                    sink.write(one_token.data(), one_token.size());
-                    return true;
-                });
+            res.set_content("{'error': '系统错误!'}\r\n\r\n", "application/json");
         });
+
+        svr_->listen("0.0.0.0", 8888);
     }
 
-    void run() {
-        wait_all_ready();
+    int inference(const std::string& text, std::string& result) {
+        std::lock_guard lk(mt_);
 
-        std::list<std::string>  history;
-        history.push_back("<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n");
+        history.push_back(system_talk);
 
-        std::string text;
-        while( readline(text) ) {
-            if ( text.size() <= 0 ) {
-                continue;
-            }
-            if ( text == "!" ) {
-                history.clear();
-                history.push_back("<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n");
-                continue;
-            }
-
-            inferencing_ = true;
-
-            std::string new_user =  "<|im_start|>user\n" + text + "<|im_end|>\n";
-            history.push_back(new_user);
-            history.push_back("<|im_start|>assistant\n");
-            std::vector<int> input_tokens = std::move( build_from_history(history) );
-
-            std::vector<int> id;
-            std::vector<int> mask;
-            for (size_t i = 0; i < input_tokens.size(); i++ ) {
-                id.push_back( input_tokens[i] );
-                mask.push_back(1);
-            }
-            mask.back() = 2;
-
-            std::vector<int> out_tokens;
-
-            auto start = std::chrono::high_resolution_clock::now();
-            int next = -1;
-            for ( int t = 0; t < (int)max_output; t++) {
-                int batch = 1;
-                int len = id.size();
-                write_all(&batch, sizeof(int));
-                write_all(&len, sizeof(int));
-                write_all(id.data(), id.size() * sizeof(int));
-                write_all(mask.data(), mask.size() * sizeof(int));
-
-                next = -1;
-                vt::CollectiveContext::pipe_read(&next, sizeof(int));
-                if ( next == tokenizer_->token_eos() ) {
-                    break;
-                }
-                out_tokens.push_back(next);
-                id.push_back(next);
-                mask.back() = 1;
-                mask.push_back(2);
-
-                std::string nstr = tokenizer_->decode(next);
-
-                writetoken(nstr);
-                printf("%s", nstr.c_str());
-                fflush(stdout);
-
-            }
-            auto stop = std::chrono::high_resolution_clock::now();
-            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-
-            std::string talk = tokenizer_->decode( out_tokens );
-            std::cout << "\n====== Generated " << out_tokens.size() << " tokens, using " << duration.count() / 1000.0 << " seconds, ";
-            std::cout << (next == tokenizer_->token_eos()) << " eos ending ===== " << std::endl;
-            // std::cout << talk << std::endl;
-
-            writetoken("");
-            inferencing_ = false;
-
-            talk = talk + "<|im_end|>\n";
-            history.push_back(talk);
+        if ( text == "!" ) {
+            history.clear();
+            history.push_back(system_talk);
         }
 
-        int n = -1;
-        write_all(&n, sizeof(int));
+        std::string new_user = text + "<|im_end|>\n";
+        history.push_back(new_user);
+        history.push_back("<|im_start|>assistant\n");
+        std::vector<int> input_tokens = std::move( build_from_history(history) );
+
+        std::vector<int> id;
+        std::vector<int> mask;
+        for (size_t i = 0; i < input_tokens.size(); i++ ) {
+            id.push_back( input_tokens[i] );
+            mask.push_back(1);
+        }
+        mask.back() = 2;
+
+        std::vector<int> out_tokens;
+
+        auto start = std::chrono::high_resolution_clock::now();
+        int next = -1;
+        for ( int t = 0; t < (int)max_output; t++) {
+            int len = id.size();
+            write_all(id.data(), id.size() * sizeof(int));
+            write_all(mask.data(), mask.size() * sizeof(int));
+
+            env_->stack().push_number(len);
+            env_->run(target_code_);
+
+            next = -1;
+            env_->ctx()->pipe_read(&next, sizeof(int));
+            if ( next == tokenizer_->token_eos() ) {
+                break;
+            }
+            out_tokens.push_back(next);
+            id.push_back(next);
+            mask.back() = 1;
+            mask.push_back(2);
+
+            std::string nstr = tokenizer_->decode(next);
+            //std::cout << nstr << std::flush;
+            printf("%s", nstr.c_str());
+            fflush(stdout);
+        }
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+
+        std::string talk = tokenizer_->decode( out_tokens );
+        std::cout << "\n====== Generated " << out_tokens.size() << " tokens, using " << duration.count() / 1000.0 << " seconds, ";
+        std::cout << (next == tokenizer_->token_eos()) << " eos ending ===== " << std::endl;
+        //std::cout << talk << std::endl;
+
+        result = talk;
+
+        talk = talk + "<|im_end|>\n";
+        history.push_back(talk);
+
+        return 0;
     }
 
     std::vector<int> build_from_history(const std::list<std::string>& history) {
@@ -173,148 +174,68 @@ struct ChatApplication {
         return all_tokens;
     }
 
-    void writetoken(std::string token) {
-        std::lock_guard lk(mt_);
-        omq_.push(token);
-        ocv_.notify_all();
-    }
+    bool readline(const std::string& prop, std::string& code) {
+        char *line = ::readline(prop.c_str());
 
-    std::string readtoken() {
-        std::unique_lock<std::mutex> lk(mt_);
-        ocv_.wait(lk);
-
-        auto token = omq_.front();
-        omq_.pop();
-        return token;
-    }
-
-    void writeline(std::string text) {
-        std::lock_guard lk(mt_);
-        imq_.push(text);
-        icv_.notify_all();
-    }
-
-    bool readline(std::string& code) {
-        std::unique_lock<std::mutex> lk(mt_);
-        icv_.wait(lk);
-
-        code = imq_.front();
-        imq_.pop();
-        if ( code == "" ) {
+        if ( line == NULL ) {
             return false;
         }
+        code = line;
+        ::add_history(line);
+        free(line);
         return true;
     }
 
+
+
 private:
-    std::mutex mt_;
-    bool inferencing_;
-
-    std::queue<std::string> imq_;
-    std::condition_variable icv_;
-    std::queue<std::string> omq_;
-    std::condition_variable ocv_;
-
     vt::Tokenizer* tokenizer_;
+    vt::Enviroment* env_;
+    vt::DaG* target_code_;
+
+    httplib::Server *svr_;
+
+    std::mutex mt_;
+
+    std::list<std::string>  history;
 };
 
-void do_inference(vt::Enviroment* env, const char* dag_file) {
-    const char* init_cmd = "gpu_init";
-    const char* main_cmd = "gpu_main";
+int main(int argc, const char* argv[] ) {
+    if ( argc < 3 ) {
+        std::cout << "usage: ./chat [dag_files] " << std::endl;
+        return -1;
+    }
+    vt::ComputingContext* ctx = new vt::ComputingContext();
+    ctx->boot_host(0);
+#ifdef _USING_DEVICE_CUDA_
+    ctx->boot_cuda(0);
+#endif
+    vt::Enviroment* env = new vt::Enviroment(ctx);
+    env->insert_native_word("app.mem", MemoryCounting::creator);
+    env->insert_native_word("app.align", MemoryAlign::creator);
+
+    // load whole code
     {
-        std::string all_code = vt::fileToString(dag_file);
+        std::string all_code;
+        for(int i = 1; i < argc; i++) {
+            const char* dag_file = argv[i];
+            all_code += fileToString(dag_file);
+        }
 
         vt::DaG* init_bin = env->build(all_code);
 #ifdef _USING_DEVICE_CUDA_
         env->stack().push_string("cuda");
-#endif
-#ifdef _USING_DEVICE_DCU_
-        env->stack().push_string("dcu");
-#endif
-#ifdef _USING_DEVICE_COREX_
-        env->stack().push_string("corex");
 #endif
         env->run(init_bin);
         delete init_bin;
     }
     env->execute(init_cmd);
 
-    int ok = 1;
-    vt_assert( vt::CollectiveContext::pipe_write(0, &ok, sizeof(int)) > 0, "pipe_write error");
+    ChatApplication* app = new ChatApplication(env);
+    app->run();
 
-    vt::DaG* target_cmd = env->build(main_cmd);
-
-    for (;;) {
-        int batches = -1;
-        int id = -1;
-        vt::CollectiveContext::pipe_read(&batches, sizeof(int));;
-        if ( batches <= 0) {
-            break;
-        }
-
-        vt::CollectiveContext::pipe_read(&id, sizeof(int));;
-        vt_assert(id > 0, "tokens can't must more than zero!");
-
-        env->stack().push_number(batches);
-        env->stack().push_number(id);
-        env->run(target_cmd);
-    }
-
-    delete target_cmd;
-}
-
-
-
-int main(int argc, char* argv[] ) {
-    if ( argc < 2 ) {
-        std::cout << "usage: ./chat [dag_file] " << std::endl;
-        return -1;
-    }
-    const char* dag_file = argv[1];
-    vt::CollectiveContext::boot_pipe(1);
-
-    if ( vt::CollectiveContext::pipe_rank == 0) {
-        httplib::Server svr;
-
-        ChatApplication* app = new ChatApplication();
-
-        std::thread tserver([&] {
-                ChatApplication::server(app, svr);
-                svr.listen("localhost", 1234);
-            });
-
-        app->run();
-
-        // close threads
-        svr.stop();
-        tserver.join();
-
-        delete app;
-        // wait for all child processes finished
-        {
-            int status = 0;
-            while ( wait(&status) != -1) {
-            }
-        }
-    } else if ( vt::CollectiveContext::pipe_rank == 1) {
-        vt::MemoryContext::boot( MEM_CTX_SIZE );
-#ifdef _USING_DEVICE_CUDA_
-        vt::ComputingContext::boot_cuda( 0 );
-#endif
-#ifdef _USING_DEVICE_COREX_
-        vt::ComputingContext::boot_corex( 0 );
-#endif
-
-        vt::Enviroment* env = new vt::Enviroment();
-        env->insert_native_word("app.mem", MemoryCounting::creator);
-        env->insert_native_word("app.align", MemoryAlign::creator);
-
-        do_inference(env, dag_file);
-
-        delete env;
-        vt::ComputingContext::shutdown();
-        vt::MemoryContext::shutdown();
-    }
-    vt::CollectiveContext::shutdown();
+    delete app;
+    delete env;
+    delete ctx;
 }
 
